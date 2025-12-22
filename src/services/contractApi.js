ce505b9b31f1a3a64154042fc6126920a2fd0bd9
Hồ Nguyên Vũ <23521805@gm.uit.edu.vn>
2025-12-22 17:52:38 +0700

    add api for student page contract

---
+++ src/services/contractApi.js
@@ -0,0 +1,16 @@
+import axios from "axios";
+
+const API_URL = `${import.meta.env.VITE_API_BASE_URL}`;
+
+export const getContractDetailById = async (contractId) => {
+  try {
+    const response = await axios.get(`${API_URL}/Contract/student-detail/${contractId}`);
+    if (response.status === 200 || response.status === 201) {
+        return { success: true, data: response.data?.data || [] };
+    }
+    return { success: false, message: response.data?.message || 'Failed to fetch contract details' };
+    }
+    catch (error) {
+        return { success: false, message: error.response?.data?.message || 'Error fetching contract details' };
+    }
+};
